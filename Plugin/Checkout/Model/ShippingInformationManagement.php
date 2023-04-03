855d1fb6cde4749ce135b60932e54f68d96313c2
Manrahul Singh <manrahul.singh@tbi.local>
2023-04-03 18:49:24 +0530

    code updated

--- Plugin/Checkout/Model/ShippingInformationManagement.php
+++ Plugin/Checkout/Model/ShippingInformationManagement.php
@@ -8,7 +8,7 @@ use Magento\Checkout\Api\Data\ShippingInformationInterface;
 class ShippingInformationManagement
 {
 
-    private $_addressInformation;
+    private $addressInformation;
     public $cartRepository;
 
     public function __construct(
@@ -16,23 +16,19 @@ class ShippingInformationManagement
         CartRepositoryInterface $cartRepository
         
     ) {
-        $this->_addressInformation = $addressInformation;
+        $this->addressInformation = $addressInformation;
         $this->cartRepository = $cartRepository;
     }
 
     public function beforeSaveAddressInformation($subject, $cartId, $addressInformation)
     {
-        $quote = $this->cartRepository->getActive($cartId);
-        $deliveryType = $addressInformation->getShippingAddress();
-        $extAttributes = $this->_addressInformation->getExtensionAttributes();
+        $extAttributes = $this->addressInformation->getExtensionAttributes()->getDeliveryType();
         $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/delivery_type.log');
         $logger = new \Zend_Log();
         $logger->addWriter($writer);
-        $logger->info(print_r($extAttributes),true);
+        $logger->info(print_r($extAttributes , true));
         $logger->info('Tbi\DeliveryType\Plugin\Quote\ShippingInformationManagement From <=');
 
-        $quote->setDeliveryType($deliveryType);
-        $this->cartRepository->save($quote);
         return [$cartId, $addressInformation];
     }
 }
--- view/frontend/web/requirejs-config.js
+++ view/frontend/web/requirejs-config.js
@@ -5,5 +5,10 @@ var config = {
                 'Tbi_CheckoutCustomField/js/action/set-shipping-information-mixin': true
             }
         }
+    },
+    "map" : {
+        "*" : {
+            "Magento_Checkout/js/action/set-shipping-information.js" : "Tbi_CheckoutCustomField/js/set-shipping-information"
+        }
     }
 };
