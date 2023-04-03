bfe931f24eaa6feefce53667650c97542fb52953
Manrahul Singh <manrahul.singh@tbi.local>
2023-04-03 18:05:31 +0530

    implemented code

--- Plugin/Checkout/Model/ShippingInformationManagement.php
+++ Plugin/Checkout/Model/ShippingInformationManagement.php
@@ -3,14 +3,20 @@
 namespace Tbi\CheckoutCustomField\Plugin\Checkout\Model;
 
 use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Checkout\Api\Data\ShippingInformationInterface;
 
 class ShippingInformationManagement
 {
+
+    private $_addressInformation;
     public $cartRepository;
 
     public function __construct(
+        ShippingInformationInterface $addressInformation,
         CartRepositoryInterface $cartRepository
+        
     ) {
+        $this->_addressInformation = $addressInformation;
         $this->cartRepository = $cartRepository;
     }
 
@@ -18,11 +24,11 @@ class ShippingInformationManagement
     {
         $quote = $this->cartRepository->getActive($cartId);
         $deliveryType = $addressInformation->getShippingAddress();
-        
+        $extAttributes = $this->_addressInformation->getExtensionAttributes();
         $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/delivery_type.log');
         $logger = new \Zend_Log();
         $logger->addWriter($writer);
-        $logger->info(print_r($addressInformation->getCustomAttributes()),true);
+        $logger->info(print_r($extAttributes),true);
         $logger->info('Tbi\DeliveryType\Plugin\Quote\ShippingInformationManagement From <=');
 
         $quote->setDeliveryType($deliveryType);
---
+++ view/frontend/web/set-shipping-information.js
@@ -0,0 +1,25 @@
+define([
+    'jquery',
+    'mage/utils/wrapper',
+    'Magento_Checkout/js/model/quote'
+], function ($, wrapper, quote) {
+    'use strict';
+
+    return function (setShippingInformationAction) {
+
+        return wrapper.wrap(setShippingInformationAction, function (originalAction) {
+            var shippingAddress = quote.shippingAddress();
+            if (shippingAddress['extension_attributes'] === undefined) {
+                shippingAddress['extension_attributes'] = {};
+            }
+            var attribute = shippingAddress.customAttributes.find(
+                function (element) {
+                    return element.attribute_code === 'delivery_type';
+                }
+            );
+            shippingAddress['extension_attributes']['delivery_type'] = attribute.value;
+            // pass execution to original action ('Magento_Checkout/js/action/set-shipping-information')
+            return originalAction();
+        });
+    };
+});
